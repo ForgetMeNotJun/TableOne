b4ea15072906100b7e63c006b893104d731fcb3e
Junichiro Shibata <58103622+ForgetMeNotJun@users.noreply.github.com>
2022-07-31 22:03:44 +0900

    [add]患者群選択機能を追加

--- main.py
+++ main.py
@@ -19,8 +19,29 @@ def splitTable(dataframe, exposure):
     df_non = dataframe[dataframe[exposure]== 0]
     return df_ex, df_non
 
-# continuous variables
-def makeContinuous(dataframe, continous_col_list):
+# choose columns
+def chooseColumns(columns_list):
+    continous_col_list = st.multiselect(
+        "Choose all continous variables that you'd like to include in Table 1.",
+        columns_list,
+        [],
+    )
+    # select categorical variables
+    categorical_col_list = st.multiselect(
+        "Choose all categorical variables that you'd like to include in Table 1.",
+        columns_list,
+        [],
+    )
+    # set columns order
+    column_order = st.multiselect(
+        "Select the columns in the order in which you'd like to order vertically.",
+        continous_col_list + categorical_col_list,
+        [],
+    )
+    return continous_col_list, categorical_col_list, column_order
+
+# create table one
+def tableOne(dataframe,continous_col_list,categorical_col_list,column_order,label):
     df_continuous = pd.DataFrame(columns=continous_col_list, index=['value'])
     for column in continous_col_list:
         #calculate each qx
@@ -30,23 +51,18 @@ def makeContinuous(dataframe, continous_col_list):
         q3 = dataframe[column].quantile(0.75).astype(float)
         #input value
         df_continuous[column].loc['value'] = f'{round(q2,1)} [{round(q1,1)}-{round(q3,1)}]'
-    return df_continuous
-
-def makeCategorical(dataframe, categorical_col_list):
+        
     df_categorical = pd.DataFrame(columns=categorical_col_list, index=['value'])
     for column in categorical_col_list:
         n = dataframe[column].sum()
         rate = n/len(dataframe) *100
         #input value
         df_categorical[column].loc['value'] = f'{int(n)} ({round(rate,1)}%)'
-    return df_categorical
-
-def tableOne(dataframe,continous_col_list,categorical_col_list,column_order,label):
-    df_continous = makeContinuous(dataframe, continous_col_list)
-    df_categorical = makeCategorical(dataframe, categorical_col_list)
-    table_one = pd.concat([df_continous,df_categorical], axis=1)
+        
+    table_one = pd.concat([df_continuous,df_categorical], axis=1)
     table_one = table_one[column_order]
     table_one = table_one.T.rename(columns={'value':label})
+    
     return table_one
 
 @st.cache
@@ -58,7 +74,7 @@ st.title('TableOne')
 
 st.header('''
 # 
-Upload the preprocessed file with which you'd like to create Table 1.
+Upload the preprocessed file from which you'd like to make Table 1.
 ''')
 
 file_type = st.selectbox(
@@ -71,47 +87,46 @@ if uploaded_file is not None:
     df = load_data(uploaded_file,file_type) 
     st.write(df.head())
     columns_list = list(df.columns)
-        
-    # select continuous variables
-    exposure = st.selectbox(
-        "On the basis of which column (exposure) would you like to divide Table 1?",
-        columns_list,
-    )
-
-    continous_col_list = st.multiselect(
-        "Choose all continous variables that you'd like to include in Table 1.",
-        columns_list,
-        []
-    )
-
-    categorical_col_list = st.multiselect(
-        "Choose all categorical variables that you'd like to include in Table 1.",
-        columns_list,
-        []
+    
+    # select 
+    table_type = st.selectbox(
+        "Do you set Exposure? (Would you like table 1 to split in multiple columns on the basis of the exposure?)",
+        ('Yes', 'No'),
     )
     
-    column_order = st.multiselect(
-        "Select the columns in the order in which you'd like to order vertically.",
-        continous_col_list + categorical_col_list,
-        []
-    )
-            
-    if len(continous_col_list +categorical_col_list)==0:
-        st.error('Select at least one column!')
+    if table_type == 'No':
+        continous_col_list, categorical_col_list, column_order = chooseColumns(columns_list)
+    
+        if len(continous_col_list +categorical_col_list)==0:
+            st.error('Select at least one column!')
+
+        table_one = tableOne(df,continous_col_list,categorical_col_list, column_order,f"overall\n(n={len(df)})")
     
     else:
+        # select an exposure
+        exposure = st.selectbox(
+            "Select an exposure variable.",
+            columns_list,
+        )
+        continous_col_list, categorical_col_list, column_order = chooseColumns(columns_list)
+        
+        if len(continous_col_list +categorical_col_list)==0:
+            st.error('Select at least one column!')
+
+        # make table one
         df_ex, df_non = splitTable(df, exposure)  
         overall_table = tableOne(df,continous_col_list,categorical_col_list, column_order,f"overall\n(n={len(df)})")
         ex_table = tableOne(df_ex,continous_col_list,categorical_col_list, column_order, f"exposure\n(n={len(df_ex)})")
         non_table = tableOne(df_non,continous_col_list,categorical_col_list, column_order,f"non-exposure\n(n={len(df_non)})")
-        
         table_one = pd.concat([overall_table,ex_table], axis=1)
         table_one = pd.concat([table_one,non_table], axis=1)
     
         p_list = []
         for column in list(table_one.index):
+            # t-test for continous
             if column in continous_col_list:
                 p = round(stats.ttest_ind(df_ex[column],df_non[column]).pvalue,2)
+            # fisher's exact test for categorical
             else:
                 A1 = len(df_ex[df_ex[column] == 1])
                 A2 = len(df_ex) - A1
@@ -122,17 +137,21 @@ if uploaded_file is not None:
                 crossTable = pd.concat([A,B])
                 x2, p, dof, e = stats.chi2_contingency(crossTable,correction=False)
             
+            # replace p-value if p<0.001
             if p >= 0.001:
                 p = str(round(p,3))
             else: p = str('<0.001')
             p_list.append(p)
         table_one['P-value'] = p_list
-        download = convert_df(table_one)
-        
-        if st.button('Make Table 1'):
-            st.table(table_one)
-            st.download_button(
-                label="Download data as csv",
-                data=download,
-                file_name='Table1.csv',
-            )
+    
+    #to_csv   
+    download = convert_df(table_one)
+     
+    #tap bottun and export csv   
+    if st.button('Make Table 1'):
+        st.table(table_one)
+        st.download_button(
+            label="Download data as csv",
+            data=download,
+            file_name='Table1.csv',
+        )
